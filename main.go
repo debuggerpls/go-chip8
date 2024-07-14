d215d6af2a1b112bad833a96e98f7ff21169567f
Deividas Puplauskas <ngivingup@gmail.com>
2024-07-14 15:30:34 +0200

    Print error message on Emulator failure
    This prints the propagated error message (such as "PC out of bounds")
    
    Signed-off-by: Deividas Puplauskas <ngivingup@gmail.com>

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"time"
 
@@ -15,16 +16,17 @@ func check(e error) {
 
 func main() {
 	// TODO: use Reader like bufio or io?
-	//data, err := os.ReadFile("/home/debuggerpls/go/src/github.com/debuggerpls/go-chip8/test_opcode.ch8")
-	data, err := os.ReadFile("/home/debuggerpls/go/src/github.com/debuggerpls/go-chip8/heart_monitor.ch8")
+	data, err := os.ReadFile("/home/debuggerpls/go/src/github.com/debuggerpls/go-chip8/test_opcode.ch8")
+	//data, err := os.ReadFile("/home/debuggerpls/go/src/github.com/debuggerpls/go-chip8/heart_monitor.ch8")
 	check(err)
 
 	emulator := chip8.Create(&chip8.DisplayTermbox{})
-	defer emulator.Destroy()
 
 	emulator.LoadProgram(data)
 
 	err = emulator.Run()
+	emulator.Destroy()
+	fmt.Println("ERROR:", err.Error())
 
 	time.Sleep(time.Second)
 }
